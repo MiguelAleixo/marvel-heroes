dab0127de3b157bcadbb5d0169679c3a50470b6f
Miguel Aleixo <miguel.lima@luizalabs.com>
2020-12-24 19:58:58 -0300

    reducers test

---
+++ src/views/HeroPage/state/__tests__/reducer.test.js
@@ -0,0 +1,111 @@
+import {
+  GET_HERO_REQUESTING,
+  GET_HERO_SUCCESS,
+  GET_HERO_FAILURE,
+  GET_COMICS_REQUESTING,
+  GET_COMICS_SUCCESS,
+  GET_COMICS_FAILURE
+} from '../constants';
+import reducer, { INITIAL_STATE } from '../reducer';
+import { hero, comics } from '../__mocks__/hero-mocks';
+
+describe('hero: reducer', () => {
+  it('should return the initial state', () => {
+    const heroReducer = reducer(undefined, {})
+
+    expect(heroReducer).toEqual(INITIAL_STATE)
+  });
+
+  it('should handle GET_HERO_REQUESTING', () => {
+    const heroReducer = reducer(
+      INITIAL_STATE,
+      { type: GET_HERO_REQUESTING }
+    )
+
+    const expected = {
+      ...INITIAL_STATE,
+      isRequesting: true
+    };
+
+    expect(heroReducer).toEqual(expected)
+  });
+
+  it('should handle GET_HERO_SUCCESS', () => {
+    const payload = hero
+
+    const heroReducer = reducer(
+      INITIAL_STATE,
+      { type: GET_HERO_SUCCESS, payload }
+    )
+
+    const expected = {
+      ...INITIAL_STATE,
+      content: payload,
+      isRequesting: false
+    }
+
+    expect(heroReducer).toEqual(expected)
+  })
+
+  it('should handle GET_HERO_FAILURE', () => {
+    const heroReducer = reducer(
+      INITIAL_STATE,
+      { type: GET_HERO_FAILURE }
+    )
+
+    const expected = {
+      ...INITIAL_STATE,
+      isRequesting: false,
+      error: true
+    }
+
+    expect(heroReducer).toEqual(expected)
+  })
+
+  it('should handle GET_COMICS_REQUESTING', () => {
+    const heroReducer = reducer(
+      INITIAL_STATE,
+      { type: GET_COMICS_REQUESTING }
+    )
+
+    const expected = {
+      ...INITIAL_STATE,
+      isRequesting: true
+    };
+
+    expect(heroReducer).toEqual(expected)
+  });
+
+  it('should handle GET_COMICS_SUCCESS', () => {
+    const payload = comics;
+
+    const heroReducer = reducer(
+      INITIAL_STATE,
+      { type: GET_COMICS_SUCCESS, payload }
+    )
+
+    const expected = {
+      ...INITIAL_STATE,
+      comics: payload,
+      isRequesting: false
+    }
+
+    expect(heroReducer).toEqual(expected)
+  })
+
+  it('should handle GET_COMICS_FAILURE', () => {
+    const heroReducer = reducer(
+      INITIAL_STATE,
+      { type: GET_COMICS_FAILURE }
+    )
+
+    const expected = {
+      ...INITIAL_STATE,
+      isRequesting: false,
+      error: true
+    }
+
+    expect(heroReducer).toEqual(expected)
+  })
+
+});
---
+++ src/views/Home/state/__tests__/reducer.test.js
@@ -0,0 +1,111 @@
+import {
+  GET_HEROES_REQUESTING,
+  GET_HEROES_SUCCESS,
+  GET_HEROES_FAILURE,
+  FAV_HERO_REQUESTING,
+  FAV_HERO_SUCCESS,
+  FAV_HERO_FAILURE
+} from '../constants';
+import reducer, { INITIAL_STATE } from '../reducer';
+import { heroes, favorites } from '../__mocks__/heroes-mocks';
+
+describe('heroes: reducer', () => {
+  it('should return the initial state', () => {
+    const heroesReducer = reducer(undefined, {})
+
+    expect(heroesReducer).toEqual(INITIAL_STATE)
+  });
+
+  it('should handle GET_HEROES_REQUESTING', () => {
+    const heroesReducer = reducer(
+      INITIAL_STATE,
+      { type: GET_HEROES_REQUESTING }
+    )
+
+    const expected = {
+      ...INITIAL_STATE,
+      isRequesting: true
+    };
+
+    expect(heroesReducer).toEqual(expected)
+  });
+
+  it('should handle GET_HEROES_SUCCESS', () => {
+    const payload = heroes
+
+    const heroesReducer = reducer(
+      INITIAL_STATE,
+      { type: GET_HEROES_SUCCESS, payload }
+    )
+
+    const expected = {
+      ...INITIAL_STATE,
+      content: payload,
+      isRequesting: false
+    }
+
+    expect(heroesReducer).toEqual(expected)
+  })
+
+  it('should handle GET_HEROES_FAILURE', () => {
+    const heroesReducer = reducer(
+      INITIAL_STATE,
+      { type: GET_HEROES_FAILURE }
+    )
+
+    const expected = {
+      ...INITIAL_STATE,
+      isRequesting: false,
+      error: true
+    }
+
+    expect(heroesReducer).toEqual(expected)
+  })
+
+  it('should handle FAV_HERO_REQUESTING', () => {
+    const heroesReducer = reducer(
+      INITIAL_STATE,
+      { type: FAV_HERO_REQUESTING }
+    )
+
+    const expected = {
+      ...INITIAL_STATE,
+      isRequesting: true
+    };
+
+    expect(heroesReducer).toEqual(expected)
+  });
+
+  it('should handle FAV_HERO_SUCCESS', () => {
+    const payload = favorites;
+
+    const heroesReducer = reducer(
+      INITIAL_STATE,
+      { type: FAV_HERO_SUCCESS, payload }
+    )
+
+    const expected = {
+      ...INITIAL_STATE,
+      favorites: payload,
+      isRequesting: false
+    }
+
+    expect(heroesReducer).toEqual(expected)
+  })
+
+  it('should handle FAV_HERO_FAILURE', () => {
+    const heroesReducer = reducer(
+      INITIAL_STATE,
+      { type: FAV_HERO_FAILURE }
+    )
+
+    const expected = {
+      ...INITIAL_STATE,
+      isRequesting: false,
+      error: true
+    }
+
+    expect(heroesReducer).toEqual(expected)
+  })
+
+});
